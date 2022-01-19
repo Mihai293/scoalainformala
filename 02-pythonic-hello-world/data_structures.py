2c8066320ea44c3553f5b4ba056a6c156bd0098a
unknown <mihai.axinte23@gmail.com>
2022-01-19 21:31:22 +0200

    mesaj

---
+++ 02-pythonic-hello-world/data_structures.py
@@ -0,0 +1,25 @@
+lista = [8, 2, -5, "string", 2.7]
+#print(type(lista))
+# print(lista[3])
+# print(lista[5]) #error
+#print(lista[1:]) #afisam elementele de la index 1 pana la finalul listei
+#print(lista[1:3])
+#print(lista[1:6])
+#print(lista[5:]) # afiseaza [] (intrebare interviu)
+#print(lista[5]) #error
+# print(lista[0:5:2]) #elementele de la index 0 pana la index 5(exclus) din 2 in 2/ pasul este setat cu 1 deff
+# print(lista[-1]) #ultimul element din lista
+# print(lista[-1:-3: -1])
+
+# lista[3] = "mere"
+# lista.append("mere")
+# print(len(lista))
+# print(lista.index("-5"))
+# print(lista.insert(3,"element nou")) #insereaza element nou
+# print (lista)
+#lista.remove(-5) #remove element -5
+# lista.pop() # scoate ultimul element din lista
+# lista.pop(2) #scoate elementul cu index 2
+# lista.clear () #lista goala
+# lista.reverse() #citire de la coada la cap
+# lista.sort()
---
+++ 02-pythonic-hello-world/main.py
@@ -0,0 +1,49 @@
+# print("Numele meu este Mihai")
+# print(1, type(-1)) # type int (intreg)
+# print(1.1, type(1.1))# type float (rational / zecimal)
+# print(1j, type(1k)) # type complex
+# print(int(1.1))
+# print(float(1))
+# print(complex(1))
+# print(1+1)
+# print(1-1)
+# print(1*0)
+# print(5/2)
+# print(5//2) # catul
+# print(5 % 2) # restul=> modulo
+# print(5 ** 2) # ridicare la putere
+
+# print(5 == 2) #operator egalitate => returneaza boolean (True sau False)
+# print(5 != 2) # operator de diferentiere => returneaza boolean (True sau False)
+# print(5 < 2)
+# print(5 > 2)
+# print(5 <= 2)
+# print(5 >= 2)
+
+#print(5 > 2 and 3>1)
+#print(5 > 2 or 3>1)
+#print(not(5 > 2 and 3 > 1)) #operator de negare
+
+#print(7 is 7)
+#print(7 is not 7)
+
+#my_var = 0
+#my_var = None
+# my_var, my_var1 = 0, 1
+#my_var = 3
+#my_var += 1
+#my_var *= 2 # assignare prin inmultire
+#print(my_var)
+
+nume = "Axinte"
+prenume = "Mihai"
+varsta = 28
+inaltime = 1.65
+#prezentare = " Numele meu este {1} si prenumele este {1}" .format (nume, prenume)
+#prezentare = f"Numele meu este {nume} si prenumele este {prenume}"
+#prezentare = "Numele meu este " + nume + " si prenumele este " + prenume + "." +  "si varsta mea este "+ str(varsta) +  "."#metoda veche
+#prezentare = f"Numele meu este {nume} si prenumele este {prenume} si varsta este {varsta}"
+#print(prezentare)
+
+calcul = nume + prenume #concatenare (str+str)
+print(calcul)
