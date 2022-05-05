8b5cd01b7c21cb95d2a6129f97268c397190d12d
unknown <mihai.axinte23@gmail.com>
2022-05-05 23:29:26 +0300

    'up'

--- mid exam/pb2.py
+++ mid exam/pb2.py
@@ -1,8 +1,11 @@
 lista =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 def pb2(lista):
     while True:
-        lista.pop()
         print(lista)
-        if lista == []:
+        lista.pop()
+        lista.pop()
+        lista.pop()
+
+        if lista == [1]:
             return ("lista = []")
 print(pb2(lista))
