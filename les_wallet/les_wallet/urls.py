327d144e6c5f6bd9e6d200c354a397f320044225
unknown <mihai.axinte23@gmail.com>
2022-05-18 15:47:32 +0300

    part2

--- les_wallet/les_wallet/urls.py
+++ les_wallet/les_wallet/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('aplicatie1.urls')),
+    path('', include('aplicatie2.urls')),
 ]
